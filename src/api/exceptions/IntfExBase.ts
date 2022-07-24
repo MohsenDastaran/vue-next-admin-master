084787306dd0c51cc46aea39908d039a076ed7dd
MohsenDastaran <mohsen.dastaran@gmail.com>
2022-07-24 23:56:02 +0430

    jwt, exceptions, storage, ejectUser & api added

---
+++ src/api/exceptions/IntfExBase.ts
@@ -0,0 +1,2 @@
+export abstract class IntfExBase extends Error {
+}
---
+++ src/modules/ejectUser.ts
@@ -0,0 +1,6 @@
+// import jwt from "@/modules/jwt"
+
+export default function () {
+	// jwt.delJwt()
+	window.location.reload()
+}
