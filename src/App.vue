e2d21cb79f92cc433cd39e9e762e4d417ee3a8a5
MohsenDastaran <mohsen.dastaran@gmail.com>
2022-07-27 02:28:35 +0430

    rtlManager added in i18n

--- src/App.vue
+++ src/App.vue
@@ -8,89 +8,75 @@
 </template>
 
 <script lang="ts">
-import { computed, ref, getCurrentInstance, onBeforeMount, onMounted, onUnmounted, nextTick, defineComponent, watch, reactive, toRefs } from 'vue';
-import { useRoute } from 'vue-router';
-import { storeToRefs } from 'pinia';
-import { useTagsViewRoutes } from '/@/stores/tagsViewRoutes';
-import { useThemeConfig } from '/@/stores/themeConfig';
-import other from '/@/utils/other';
-import { Local, Session } from '/@/utils/storage';
-import setIntroduction from '/@/utils/setIconfont';
-import LockScreen from '/@/layout/lockScreen/index.vue';
-import Setings from '/@/layout/navBars/breadcrumb/setings.vue';
-import CloseFull from '/@/layout/navBars/breadcrumb/closeFull.vue';
+import { computed, ref, getCurrentInstance, onBeforeMount, onMounted, onUnmounted, nextTick, defineComponent, watch, reactive, toRefs } from 'vue'
+import { useRoute } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { useTagsViewRoutes } from '/@/stores/tagsViewRoutes'
+import { useThemeConfig } from '/@/stores/themeConfig'
+import other from '/@/utils/other'
+import { Local, Session } from '/@/utils/storage'
+import setIntroduction from '/@/utils/setIconfont'
+import LockScreen from '/@/layout/lockScreen/index.vue'
+import Setings from '/@/layout/navBars/breadcrumb/setings.vue'
+import CloseFull from '/@/layout/navBars/breadcrumb/closeFull.vue'
 
 export default defineComponent({
 	name: 'app',
 	components: { LockScreen, Setings, CloseFull },
 	setup() {
-		const { proxy } = <any>getCurrentInstance();
-		const setingsRef = ref();
-		const route = useRoute();
-		const stores = useTagsViewRoutes();
-		const storesThemeConfig = useThemeConfig();
-		const { themeConfig } = storeToRefs(storesThemeConfig);
+		const { proxy } = <any>getCurrentInstance()
+		const setingsRef = ref()
+		const route = useRoute()
+		const stores = useTagsViewRoutes()
+		const storesThemeConfig = useThemeConfig()
+		const { themeConfig } = storeToRefs(storesThemeConfig)
 		const state = reactive({
-			i18nLocale: null,
-		});
-		// 获取全局组件大小
+			i18nLocale: null
+		})
 		const getGlobalComponentSize = computed(() => {
-			return other.globalComponentSize();
-		});
-		// 布局配置弹窗打开
+			return other.globalComponentSize()
+		})
 		const openSetingsDrawer = () => {
-			setingsRef.value.openDrawer();
-		};
-		// 设置初始化，防止刷新时恢复默认
+			setingsRef.value.openDrawer()
+		}
 		onBeforeMount(() => {
-			// 设置批量第三方 icon 图标
-			setIntroduction.cssCdn();
-			// 设置批量第三方 js
-			setIntroduction.jsCdn();
-		});
-		// 页面加载时
+			setIntroduction.cssCdn()
+			setIntroduction.jsCdn()
+		})
 		onMounted(() => {
 			nextTick(() => {
-				// 监听布局配置弹窗点击打开
-				proxy.mittBus.on('openSetingsDrawer', () => {
-					openSetingsDrawer();
-				});
-				// 设置 i18n，App.vue 中的 el-config-provider
+				proxy.mittBus.on('openSetingsDrawer', () => {})
 				proxy.mittBus.on('getI18nConfig', (locale: string) => {
-					(state.i18nLocale as string | null) = locale;
-				});
-				// 获取缓存中的布局配置
+					;(state.i18nLocale as string | null) = locale
+				})
 				if (Local.get('themeConfig')) {
-					storesThemeConfig.setThemeConfig(Local.get('themeConfig'));
-					document.documentElement.style.cssText = Local.get('themeConfigStyle');
+					storesThemeConfig.setThemeConfig(Local.get('themeConfig'))
+					document.documentElement.style.cssText = Local.get('themeConfigStyle')
 				}
-				// 获取缓存中的全屏配置
 				if (Session.get('isTagsViewCurrenFull')) {
-					stores.setCurrenFullscreen(Session.get('isTagsViewCurrenFull'));
+					stores.setCurrenFullscreen(Session.get('isTagsViewCurrenFull'))
 				}
-			});
-		});
-		// 页面销毁时，关闭监听布局配置/i18n监听
+			})
+		})
 		onUnmounted(() => {
-			proxy.mittBus.off('openSetingsDrawer', () => {});
-			proxy.mittBus.off('getI18nConfig', () => {});
-		});
-		// 监听路由的变化，设置网站标题
+			proxy.mittBus.off('openSetingsDrawer', () => {})
+			proxy.mittBus.off('getI18nConfig', () => {})
+		})
 		watch(
 			() => route.path,
 			() => {
-				other.useTitle();
+				other.useTitle()
 			},
 			{
-				deep: true,
+				deep: true
 			}
-		);
+		)
 		return {
 			themeConfig,
 			setingsRef,
 			getGlobalComponentSize,
-			...toRefs(state),
-		};
-	},
-});
+			...toRefs(state)
+		}
+	}
+})
 </script>
